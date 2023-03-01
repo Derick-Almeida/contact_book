bd61872d01c7ee16116de37d6df17068d0789744
Derick <d.a.s2000silva14@gmail.com>
2023-03-01 19:56:50 -0300

    fix: authorization login and remove list users route

--- project/backend/routes/user_routes.py
+++ project/backend/routes/user_routes.py
@@ -5,7 +5,6 @@ from project.backend.controllers import UserControllers
 def user_routes(app: Flask):
     app.post("/api/login")(UserControllers.user_login)
     app.post("/api/users")(UserControllers.create_user)
-    app.get("/api/users")(UserControllers.list_users)
     app.get("/api/users/<id>")(UserControllers.retrieve_user)
     app.patch("/api/users/<id>")(UserControllers.update_user)
     app.delete("/api/users/<id>")(UserControllers.delete_user)
--- project/backend/utils/validate_token.py
+++ project/backend/utils/validate_token.py
@@ -6,8 +6,8 @@ import os
 def validate_token(token: str, output: bool = False):
     try:
         if output:
-            return decode(token, os.getenv("SECRET_KEY"), ["HS256"])
-        decode(token, os.getenv("SECRET_KEY"), ["HS256"])
+            return decode(token, key=os.getenv("SECRET_KEY"), algorithms=["HS256"])
+        decode(token, key=os.getenv("SECRET_KEY"), algorithms=["HS256"])
 
     except exceptions.DecodeError:
         return jsonify({"message": "Invalid Token"}), 401
@@ -17,5 +17,8 @@ def validate_token(token: str, output: bool = False):
 
 
 def verify_token_middleware():
-    token = request.headers["Authorization"].split(" ")[-1]
-    return validate_token(token)
+    try:
+        token = request.headers["Authorization"].split(" ")[-1]
+        return validate_token(token)
+    except KeyError:
+        return jsonify({"message": "Missing Token"}), 401
